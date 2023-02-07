dfe0cf7e90970b64df5b76e070d0ee91a10acc9c
2531yasuaki@gmail.com <2531yasuaki@gmail.com>
2023-02-07 18:46:03 +0900

    Done step06

--- steps/common.py
+++ steps/common.py
@@ -1,30 +1,46 @@
+# coding: utf-8
+"""共通の関数やクラスを定義するモジュール"""
 import numpy as np
+
 
 class Variable:
     """変数を表すクラス
 
     Attributes:
         data (ndarray): 変数の中身
+        grad (ndarray): 微分値
     """
 
     def __init__(self, data):
+        """コンストラクタ
+
+        Args:
+            data (ndarray): 変数の中身
+        """
         self.data = data
+        self.grad = None  # 微分値を保持する変数
 
 
 class Function:
     """関数を表すクラス
 
-    Args:
+    Attributes:
         input (Variable): 入力
-
-    Returns:
-        Variable: 関数を適用した出力
     """
 
     def __call__(self, input):  # 引数を与えられるとこのメソッドが呼び出される
+        """関数を呼び出したときの処理
+
+        Args:
+            input (Variable): 入力
+
+        Returns:
+            Variable: 関数を適用した出力
+        """
         x = input.data  # データを取り出す
         y = self.forward(x)  # 具体的な計算を呼び出す
         output = Variable(y)  # Variableとして返す
+        self.input = input  # backwardのために入力を覚えておく
         return output
 
     def forward(self, x):
@@ -41,32 +57,84 @@ class Function:
         """
         raise NotImplementedError()
 
+    def backward(self, gy):
+        """微分を計算する
+
+        Args:
+            gy (ndarray): 出力に対する微分値
+
+        Returns:
+            ndarray: 入力に対する微分値
+
+        Raises:
+            NotImplementedError: 実装されていない場合
+        """
+        raise NotImplementedError()
+
 
 class Square(Function):
     """2乗を計算する関数を表すクラス
 
-    Args:
+    Attributes:
         input (Variable): 入力
-
-    Returns:
-        Variable: 2乗を適用した出力
     """
 
     def forward(self, x):
-        return x**2  # 具体的な計算を実装するだけ
+        """2乗を計算する
+
+        Args:
+            x (ndarray): 入力
+
+        Returns:
+            ndarray: 2乗を適用した出力
+        """
+        y = x**2
+        return y  # 具体的な計算を実装するだけ
+
+    def backward(self, gy):
+        """微分を計算する
+
+        Args:
+            gy (ndarray): 出力に対する微分値
+
+        Returns:
+            ndarray: 入力に対する微分値
+        """
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 
 class Exp(Function):
-    """指数関数を計算する関数
+    """指数関数を計算する関数を表すクラス
 
-    Args:
+    Attributes:
         input (Variable): 入力
-
-    Returns:
-        Variable: 指数関数を適用した出力
     """
+
     def forward(self, x):
+        """指数関数を計算する
+
+        Args:
+            x (ndarray): 入力
+
+        Returns:
+            ndarray: 指数関数を適用した出力
+        """
         return np.exp(x)
+
+    def backward(self, gy):
+        """微分を計算する
+
+        Args:
+            gy (ndarray): 出力に対する微分値
+
+        Returns:
+            ndarray: 入力に対する微分値
+        """
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 
 def numerical_diff(f, x, eps=1e-4):
---
+++ steps/step06.py
@@ -0,0 +1,19 @@
+# 06. 手作業によるバックプロパゲーション
+import numpy as np
+from common import Exp, Square, Variable
+
+if __name__ == "__main__":
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)  # 逆伝播の初期値は1
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+    print(x.grad)
