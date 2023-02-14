df56d52bf849b42d93905201fdd0866ed6920531
2531yasuaki@gmail.com <2531yasuaki@gmail.com>
2023-02-15 03:05:30 +0900

    unittestからpytestに変更

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,30 +1,33 @@
 import sys
-import unittest
+import pytest
 
-sys.path.append("..")
+sys.path.append(".")
 import numpy as np
 
 from steps.common import Variable, numerical_diff, square
 
 
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)  # 期待した値と出力が一致するか検証
+def test_forward():
+    x = Variable(np.array(2.0))
+    y = square(x)
+    expected = np.array(4.0)
+    assert y.data == expected  # 期待した値と出力が一致するか検証
 
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
+def test_backward():
+    x = Variable(np.array(3.0))
+    y = square(x)
+    y.backward()
+    expected = np.array(6.0)
+    assert x.grad == expected
 
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))  # ランダムな入力値を生成
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)  # 2つが近い値かを判定
-        self.assertTrue(flg)
+def test_gradient_check():
+    x = Variable(np.random.rand(1))  # ランダムな入力値を生成
+    y = square(x)
+    y.backward()
+    num_grad = numerical_diff(square, x)
+    flg = np.allclose(x.grad, num_grad)  # 2つが近い値かを判定
+    assert flg
+
+
+if __name__ == "__main__":
+    pytest.main()
